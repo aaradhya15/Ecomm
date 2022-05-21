2a6e96d7f0f517895bd85f014b3176ec360ab3e2
aaradhya15 <jainaaru1501@gmail.com>
2022-05-21 16:59:13 +0530

    replaced dummy data by dynamic data in checkout page

--- ecomm/store/views.py
+++ ecomm/store/views.py
@@ -13,6 +13,7 @@ def cart(request):
           order, created = Order.objects.get_or_create(customer = customer, complete = False)
           items = order.orderitem_set.all()
      else:
+          #creating empty cart for now for non-logged in users
           items=[]
           order = {'get_cart_total':0, 'get_cart_items':0}
 
@@ -20,5 +21,14 @@ def cart(request):
      return render(request, 'store/cart.html', context)
 
 def checkout(request):
-      context = {}
-      return render(request, 'store/checkout.html', context)
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer = customer, complete = False)
+          items = order.orderitem_set.all()
+     else:
+          #creating empty cart for now for non-logged in users
+          items=[]
+          order = {'get_cart_total':0, 'get_cart_items':0}
+
+     context = {'items' : items, 'order':order}
+     return render(request, 'store/checkout.html', context)
