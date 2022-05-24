451c4b67fe16798fbf360c33123d45a7801dad1f
aaradhya15 <jainaaru1501@gmail.com>
2022-05-24 23:11:14 +0530

    added some order processing logic

--- ecomm/store/views.py
+++ ecomm/store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
-import json
+import json,datetime
 
 # Create your views here.
 def store(request):
@@ -43,6 +43,7 @@ def checkout(request):
      context = {'items' : items, 'order':order}
      return render(request, 'store/checkout.html', context)
 
+# api to update cart value and items
 def updateItem(request):
      data = json.loads(request.body)
      productId = data['productId']
@@ -66,3 +67,31 @@ def updateItem(request):
      if orderItem.quantity<=0:
           orderItem.delete()
      return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+     transaction_id = datetime.datetime.now().timestamp()
+     data = json.loads(request.body)
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer = customer, complete = False)
+          total = float(data['form']['total'])
+          order.transaction_id = transaction_id
+
+          if total == order.get_cart_total:
+               order.complete = True
+          order.save()
+
+          if order.shipping == True:
+               ShippingAddress.objects.create(
+                    customer =customer,
+                    order = order,
+                    address = data['shipping']['address'],
+                    city = data['shipping']['city'],
+                    state = data['shipping']['state'],
+                    zipcode = data['shipping']['zip'],
+
+               )
+
+     else:
+          print('user not logged in')
+     return JsonResponse('Payment submitted...', safe=False)
